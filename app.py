00343adf33338248ddd741213a9c12139866eb8c
BorissowT <tbs999borisov@gmail.com>
2020-11-21 16:13:11 +0300

    add login interface

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from db.models import session
-from db.models import Station
+from db.models import Organisation
+from interface.login_interface import Ui_MainWindow,QtWidgets
 
+# new_ex = Organisation(Title="NEW")
+# session.add(new_ex)
+# session.commit()
+#
+# results = session.query(Organisation).all()
+# list = list(map(lambda elem: elem.Title, results))
+# print(list)
 
-results = session.query(Station).all()
-
-
-list = list(map(lambda elem: elem.Title, results))
-
-print(list)
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
